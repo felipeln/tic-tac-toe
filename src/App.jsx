94bad7bee0c2548232bc3f8bc5f2c5775b6d4657
Felipeln <76855578+felipeln@users.noreply.github.com>
2023-08-14 11:17:47 -0300

    add winners squares highlight

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
 import "./App.css";
-
-function Square({ value, onSquareClick }) {
-  // definindo o nome das props que serão passadas
-  const squareClass = `square ${value}`
-  return (
-    <button className={squareClass} onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+// definindo o nome das props que serão passadas
+function Square({ value, onSquareClick, linesWinner, dataIndex }) {
+  // pegando o N° das linhas vencedoras
+  const lines = linesWinner;
+  // verificando se ja temos o vencedor
+  if (lines.length > 0) {
+    // se sim, separe o  N das linha em 3 variaveis.
+    let [a, b, c] = lines;
+    // se o dataIndex(numero da posição do square) for igual ao Numero de algum square Vencedor.
+    if (a === dataIndex || b === dataIndex || c === dataIndex) {
+      // retorna ele com uma class "winner"
+      return (
+        <button className={`square ${value} winner`} onClick={onSquareClick}>
+          {value}
+        </button>
+      );
+    } else { // caso não, retorne ele normal
+      return (
+        <button className={`square ${value}`} onClick={onSquareClick}>
+          {value}
+        </button>
+      );
+    }
+  }
+  // caso nao tenha um vencedor, renderize normal
+  if (lines.length < 1) { 
+    return (
+      <button className={`square ${value}`} onClick={onSquareClick}>
+        {value}
+      </button>
+    );
+  }
 }
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    // se o square ja tiver um valor, retorne este mesmo valor e nao mude ele.
+    // verifica se tme um ganhador
+    // ou
+    // se o squares[i], ja tem um valor.
+    // caso alguma seja truthy, pare por aqui.
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -30,38 +56,48 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   // verificando se tem um ganhador
-  const winner = calculateWinner(squares);
+  const winner = calculateWinner(squares); // retorna um array [ com o vencedor "X/O" e um array com Numero das posiçoes vencedoras]
   let status;
-  if(winner) {
-    status = "Winner: " + winner;
-  } if(!winner) {
+  let linesWinner = [];
+  if (winner) {
+    status = "Winner: " + winner[0]; // winner[0]
+    linesWinner = winner[1];
+  }
+  if (!winner) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  // console.log(linesWinner);
+
+  // using loop to make squares
+  const rowGroups = [];
+  let currentGroup = [];
+  const currentSquares = squares;
+  currentSquares.forEach((item, index, array) => {
+    currentGroup.push(
+      <Square
+        key={`square-${index}`}
+        dataIndex={index}
+        linesWinner={[...linesWinner]}
+        value={squares[index]}
+        onSquareClick={() => handleClick(index)}
+      />
+    );
+
+    if (currentGroup.length === 3 || index === array.length - 1) {
+      rowGroups.push(
+        <div className="board-row" key={`row-${rowGroups.length + 1}`}>
+          {currentGroup}
+        </div>
+      );
+      currentGroup = [];
+    }
+  });
 
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-container">
-        <div className="board-row">
-          {/* passando as props, 'value' e 'onSquareClick' */}
-          {/* squares[0] é onde 'X' ou 'O' vai ser colocado */}
-          {/* handleClick(0) é onde precisa ser clicado para que o 'X' ou 'O' seja colocado */}
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </div>
+      <div className=".board-container">{rowGroups}</div>
     </>
   );
 }
@@ -81,18 +117,19 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  let linesWinner;
   // passa por cada array de linhas
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]; // pega o valor de cada item da linha
     // verifica se tem algum valor e se estiver verifica se ele é igual aos outros valores.
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      linesWinner = lines[i];
+      return [squares[a], linesWinner];
     }
   }
-  // se estiver um vencedor ou todos estiverem preenchidos retorna null
+  // se tem um vencedor ou todos tem  estão preenchidos retorna null
   return null;
 }
-
 
 function Game() {
   // guarda o historico de açoes
@@ -102,16 +139,20 @@ function Game() {
   const currentSquares = history[currentMove];
   const xIsNext = currentMove % 2 === 0;
 
+  // função para modificar estados do historico
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     sethistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
+  // função para pular para alguma jogada, baseado no historico.
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
+  // cria os botoes "go start" e "go to move"
+  // baseado no historico
   const moves = history.map((squares, move) => {
     let desc;
     if (move > 0) {
@@ -120,7 +161,7 @@ function Game() {
     if (move === 0) {
       desc = `Go to game Start`;
     }
-
+ 
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{desc}</button>
@@ -128,24 +169,30 @@ function Game() {
     );
   });
 
+  // TODO: Add a toggle button that lets you sort the moves in either ascending or descending order.
+
+
   return (
     <>
-    <h1 className="title">Tic Tac Toe</h1>
-    <div className="game-container">
-      <div className="game">
-        <div className="game-board">
-          <Board
-            xIsNext={xIsNext}
-            squares={currentSquares}
-            onPlay={handlePlay}
-          />
+      <h1 className="title">Tic Tac Toe</h1>
+      <div className="game-container">
+        <div className="game">
+          <div className="game-board">
+            <Board
+              xIsNext={xIsNext}
+              squares={currentSquares}
+              onPlay={handlePlay}
+            />
+          </div>
+        </div>
+        <div className="game-info">
+          <h2>Game history</h2>
+          <button  className="sortButton" aria-label="Sort by Order">
+            {`Sort Order`}
+          </button>
+          <ol>{moves}</ol>
         </div>
       </div>
-      <div className="game-info">
-        <h2>Game history</h2>
-        <ol>{moves}</ol>
-      </div>
-    </div>
     </>
   );
 }
